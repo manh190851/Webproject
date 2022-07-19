15beb41cc76e9af460b2b9138bf0c5270a0f0c78
Manh <phammanh693@gmail.com>
2022-07-19 19:34:13 +0700

    create-user-and-role-model

---
+++ Webproject.Data/EntityModel/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webproject.Data.EntityModel
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string fullname { get; set; }
+        public string gender { get; set; }
+        public DateTime DOB { get; set; }
+        public string phonenumber { get; set; }
+        public string nation { get; set; }
+        public string city { get; set; }
+        public string imageUrl { get; set; }
+
+    }
+}
